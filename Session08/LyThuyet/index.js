8c4f5e4f4aa2ced679bcf55d76540de482904cf7
hoangpham-hanoi <hoangpham.0885@gmail.com>
2024-07-31 08:17:25 +0700

    homework lesson 08

---
+++ Session08/LyThuyet/index.js
@@ -0,0 +1,18 @@
+document.write("Rikkei Academy")
+// khai báo biến: var, let, const
+// khai báo tuổi 1 người
+var age = 18;
+age = 20; 
+console.log("age: ", age)
+let fullName = "Vũ Hồng Vân";
+let letCheckEmail;
+let isBoolean = 5>4;
+console.log(isBoolean);
+console.log("tên của bạn là: ", fullName)
+let checkIsNull = null;
+let checkIsNaN = "JS" - 5;
+console.log(111111, checkIsNaN);
+const age = 20;
+// age = 30;
+/* alt + shift + a */
+// ctrl + /
